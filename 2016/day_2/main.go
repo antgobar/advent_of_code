8a876ca343d360f18636e8d15df79a12b730510b
antgobar <antongb09@gmail.com>
2024-11-19 22:05:13 +0000

    day 2 complete

--- 2016/day_2/main.go
+++ 2016/day_2/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"os"
-	"strconv"
 	"strings"
 )
 
@@ -15,92 +14,118 @@ func ReadTextFile(path string) []string {
 	return strings.Split(string(content), "\n")
 }
 
+func (c Coordinates) Movement(direction string, numPad NumPad) Coordinates {
+	var newCoords Coordinates
+	bounds := numPad.Bounds()
+	fmt.Println(bounds)
+	switch direction {
+	case "U":
+		newCoords = Coordinates{c.x, c.y + 1}
+		if newCoords.y > bounds.yMax {
+			return Coordinates{newCoords.x, bounds.yMax}
+		}
+		if numPad.ValueAtCoord(newCoords) == " " {
+			return c
+		}
+		return newCoords
+	case "D":
+		newCoords = Coordinates{c.x, c.y - 1}
+		if newCoords.y < bounds.yMin {
+			return Coordinates{newCoords.x, bounds.yMin}
+		}
+		if numPad.ValueAtCoord(newCoords) == " " {
+			return c
+		}
+		return newCoords
+	case "L":
+		newCoords = Coordinates{c.x - 1, c.y}
+		if newCoords.x < bounds.xMin {
+			return Coordinates{bounds.xMin, newCoords.y}
+		}
+		if numPad.ValueAtCoord(newCoords) == " " {
+			return c
+		}
+		return newCoords
+	case "R":
+		newCoords = Coordinates{c.x + 1, c.y}
+		if newCoords.x > bounds.xMax {
+			return Coordinates{bounds.xMax, newCoords.y}
+		}
+		if numPad.ValueAtCoord(newCoords) == " " {
+			return c
+		}
+		return newCoords
+	default:
+		return c
+	}
+}
+
+func getDigit(line string, coordinate Coordinates, numPad NumPad) Coordinates {
+	actions := strings.Split(line, "")
+
+	for _, action := range actions {
+		coordinate = coordinate.Movement(action, numPad)
+	}
+	return coordinate
+}
+
+type Digits []string
+
+func main() {
+	data := ReadTextFile("day_2.txt")
+
+	var digits1 Digits
+	var startCoordinate1 = Coordinates{1, 1}
+	numberPad1 := NumPad{
+		{"1", "2", "3"},
+		{"4", "5", "6"},
+		{"7", "8", "9"},
+	}
+
+	var digits2 Digits
+	var startCoordinate2 = Coordinates{0, 2}
+	numberPad2 := NumPad{
+		{" ", " ", "1", " ", " "},
+		{" ", "2", "3", "4", " "},
+		{"5", "6", "7", "8", "9"},
+		{" ", "A", "B", "C", " "},
+		{" ", " ", "D", " ", " "},
+	}
+
+	for _, line := range data {
+		coordinate1 := getDigit(line, startCoordinate1, numberPad1)
+		startCoordinate1 = coordinate1
+		digits1 = append(digits1, numberPad1.ValueAtCoord(coordinate1))
+
+		coordinate2 := getDigit(line, startCoordinate2, numberPad2)
+		startCoordinate2 = coordinate2
+		digits2 = append(digits2, numberPad2.ValueAtCoord(coordinate2))
+	}
+
+	fmt.Println("Problem 1:", strings.Join(digits1, ""))
+	fmt.Println("Problem 2:", strings.Join(digits2, ""))
+
+}
+
 type Coordinates struct {
 	x, y int
 }
 
-func (c Coordinates) Movement(direction string) Coordinates {
-	var newCoords Coordinates
-	switch direction {
-	case "U":
-		newCoords = Coordinates{c.x, c.y + 1}
-	case "D":
-		newCoords = Coordinates{c.x, c.y - 1}
-	case "L":
-		newCoords = Coordinates{c.x - 1, c.y}
-	case "R":
-		newCoords = Coordinates{c.x + 1, c.y}
-	default:
-		newCoords = c
-	}
-	if newCoords.x < 0 {
-		return Coordinates{0, newCoords.y}
-	}
-	if newCoords.x > 2 {
-		return Coordinates{2, newCoords.y}
-	}
-	if newCoords.y < 0 {
-		return Coordinates{newCoords.x, 0}
-	}
-	if newCoords.y > 2 {
-		return Coordinates{newCoords.x, 2}
-	}
-	return newCoords
+type NumPad [][]string
+
+type NumPadBounds struct {
+	xMax, xMin, yMax, yMin int
 }
 
-func (c Coordinates) AsDigit() int {
-	switch c {
-	case Coordinates{0, 0}:
-		return 7
-	case Coordinates{0, 1}:
-		return 8
-	case Coordinates{0, 2}:
-		return 9
-	case Coordinates{1, 0}:
-		return 4
-	case Coordinates{1, 1}:
-		return 5
-	case Coordinates{1, 2}:
-		return 6
-	case Coordinates{2, 0}:
-		return 1
-	case Coordinates{2, 1}:
-		return 2
-	case Coordinates{2, 2}:
-		return 3
-	default:
-		return 0
-	}
+func (np NumPad) ValueAtCoord(c Coordinates) string {
+	x := max(0, min(c.x, len(np[0])-1))
+	y := max(0, min(len(np)-1-c.y, len(np)-1))
+	return np[y][x]
 }
 
-func getDigit(line string) int {
-	actions := strings.Split(line, "")
-	var coordinate = Coordinates{1, 1}
-
-	for _, action := range actions {
-		coordinate = coordinate.Movement(action)
+func (np NumPad) Bounds() NumPadBounds {
+	return NumPadBounds{
+		xMax: len(np[0]) - 1,
+		yMax: len(np) - 1,
 	}
-	return coordinate.AsDigit()
-}
-
-type Digits []int
-
-func (d Digits) Combine() string {
-	stringNumbers := make([]string, len(d))
-	for i, num := range d {
-		stringNumbers[i] = strconv.Itoa(num)
-	}
-	return strings.Join(stringNumbers, "")
-}
-
-func main() {
-	data := ReadTextFile("day_2_test.txt")
-	var digits Digits
-
-	for _, line := range data {
-		digit := getDigit(line)
-		digits = append(digits, digit)
-	}
-
-	fmt.Println("Problem 1:", digits.Combine())
 }
