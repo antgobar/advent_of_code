09a8c1f745290c56b19d022d26d63c938623237e
antgobar <antongb09@gmail.com>
2024-11-22 16:54:06 +0000

    day 4

--- 2016/day_4/main.go
+++ 2016/day_4/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"sort"
 	"strconv"
 	"strings"
 )
@@ -31,29 +32,77 @@ func ParseLineToRoom(line string) Room {
 	if err != nil {
 		fmt.Println("Error:", err)
 	}
-	roomName := strings.Join(splitRoomNameId[:len(splitRoomNameId)-1], "")
+	roomName := strings.Join(splitRoomNameId[:len(splitRoomNameId)-1], "-")
 	checkSum := preCheckSum[:len(preCheckSum)-1]
 
 	return Room{ID: roomId, Name: roomName, CheckSum: checkSum}
 }
 
+type Frequency struct {
+	char  rune
+	count int
+}
+
 func (r Room) GenerateCheckSum() string {
-	for _, char := range strings.Split(r.Name, "") {
-		fmt.Println(char)
+	char_frequencies := make(map[rune]int)
+	var frequencies []Frequency
+
+	roomName := strings.ReplaceAll(r.Name, "-", "")
+
+	for _, char := range roomName {
+		char_frequencies[char]++
 	}
 
-	return "foo"
+	for char, count := range char_frequencies {
+		frequencies = append(frequencies, Frequency{char, count})
+	}
+
+	sort.Slice(frequencies, func(i, j int) bool {
+		if frequencies[i].count != frequencies[j].count {
+			return frequencies[i].count > frequencies[j].count
+		}
+		return frequencies[i].char < frequencies[j].char
+	})
+
+	result := make([]rune, 0, 5)
+	for i := 0; i < 5 && i < len(frequencies); i++ {
+		result = append(result, frequencies[i].char)
+	}
+
+	return string(result)
 }
 
 func (r Room) IsReal() bool {
 	return r.GenerateCheckSum() == r.CheckSum
 }
 
+func (r Room) RotN() string {
+	result := make([]rune, 0, len(r.Name))
+
+	for _, char := range r.Name {
+		var rotated rune
+		if char >= 'A' && char <= 'Z' {
+			rotated = 'A' + (char-'A'+rune(r.ID))%26
+		} else if char >= 'a' && char <= 'z' {
+			rotated = 'a' + (char-'a'+rune(r.ID))%26
+		} else {
+			rotated = char
+		}
+
+		result = append(result, rotated)
+	}
+
+	return string(result)
+}
+
 func main() {
-	data := ReadTextFile("day_4_test.txt")
+	data := ReadTextFile("day_4.txt")
 	count := 0
 	for _, line := range data {
 		room := ParseLineToRoom(line)
+		if room.RotN() == "northpole-object-storage" {
+			fmt.Println("Problem 2:", room.ID)
+		}
 		if room.IsReal() {
 			count += room.ID
 		}
