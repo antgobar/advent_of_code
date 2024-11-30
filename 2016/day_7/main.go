435966f1dde704497deddd1a78f61b15ae1ee4b6
antgobar <antongb09@gmail.com>
2024-11-30 16:55:47 +0000

    Starting day 7

--- 2016/day_7/main.go
+++ 2016/day_7/main.go
@@ -1,1 +1,24 @@
 package main
+
+import (
+	"fmt"
+)
+
+func isPalindrome(s string) bool {
+	runes := []rune(s)
+	reverse_container := make([]rune, len(runes))
+
+	for i, letter := range runes {
+		reverse_container[len(s)-1-i] = rune(letter)
+	}
+
+	return s == string(reverse_container)
+}
+
+func main() {
+	text1 := "abba"
+	text2 := "abcd"
+
+	fmt.Println(isPalindrome(text1), isPalindrome(text2))
+
+}
