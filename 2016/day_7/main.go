f3e71b6bf8ef273a6fc5e34e63120bce308d95f9
antgobar <antongb09@gmail.com>
2024-12-01 10:54:07 +0000

    2024 start

--- 2016/day_7/main.go
+++ 2016/day_7/main.go
@@ -2,7 +2,18 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"regexp"
+	"strings"
 )
+
+func ReadTextFile(path string) []string {
+	content, err := os.ReadFile(path)
+	if err != nil {
+		panic(fmt.Sprintf("Error reading file: %v", err))
+	}
+	return strings.Split(string(content), "\n")
+}
 
 func isPalindrome(s string) bool {
 	runes := []rune(s)
@@ -15,10 +26,74 @@ func isPalindrome(s string) bool {
 	return s == string(reverse_container)
 }
 
+func containsValidPalindrome(str string, size int) bool {
+	if size <= 0 || len(str) < size {
+		return false
+	}
+
+	for i := 0; i <= len(str)-size; i++ {
+		n_substring := str[i : i+size]
+		if isPalindrome(n_substring) && !allCharsTheSame(n_substring) {
+			return true
+		}
+	}
+	return false
+}
+
+type BracketGroups struct {
+	inside  []string
+	outside []string
+}
+
+func HasPalindrome(strings []string) bool {
+	for _, str := range strings {
+		if containsValidPalindrome(str, 4) {
+			return true
+		}
+	}
+	return false
+}
+
+func extractStrings(inputString string) ([]string, []string) {
+	reInside := regexp.MustCompile(`\[(.*?)\]`)
+	insideBrackets := reInside.FindAllStringSubmatch(inputString, -1)
+	var inside []string
+	for _, match := range insideBrackets {
+		inside = append(inside, match[1])
+	}
+
+	reOutside := regexp.MustCompile(`\[[^\[\]]*\]`)
+	outsideParts := reOutside.Split(inputString, -1)
+	var outside []string
+	for _, part := range outsideParts {
+		if strings.TrimSpace(part) != "" {
+			outside = append(outside, part)
+		}
+	}
+	return inside, outside
+}
+
+func allCharsTheSame(strs string) bool {
+	first := rune(strs[0])
+
+	for _, s := range strs {
+		if first != s {
+			return false
+		}
+	}
+	return true
+}
+
 func main() {
-	text1 := "abba"
-	text2 := "abcd"
+	data := ReadTextFile("day_7.txt")
+	valid := 0
 
-	fmt.Println(isPalindrome(text1), isPalindrome(text2))
+	for _, line := range data {
+		inside, outside := extractStrings(line)
+		if !HasPalindrome(inside) && HasPalindrome(outside) {
+			valid++
+		}
+	}
 
+	fmt.Println(valid)
 }
