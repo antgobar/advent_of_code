27d1e3386e043e4c05ad9a199ff4992a2dd74899
antgobar <antongb09@gmail.com>
2024-12-02 20:10:16 +0000

    2024 day 2

--- 2024/day_2/main.go
+++ 2024/day_2/main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -15,18 +17,94 @@ func ReadFile(path string) []string {
 	return strings.Split(string(content), "\n")
 }
 
-func ReportIsSafe(report []string) bool {
+type Report []int
+
+func (report Report) isOrdered() bool {
+	if len(report) <= 1 {
+		return true
+	}
+
+	ascending := true
+	descending := true
+
+	for i := 1; i < len(report); i++ {
+		if report[i] <= report[i-1] {
+			ascending = false
+		}
+		if report[i] >= report[i-1] {
+			descending = false
+		}
+	}
+	return ascending || descending
+}
+
+func (report Report) HasMaxDifference(n int) bool {
+	for i := 1; i < len(report); i++ {
+		numA := float64(report[i])
+		numB := float64(report[i-1])
+
+		if int(math.Abs(numA-numB)) > n {
+			return false
+		}
+	}
 	return true
 }
 
-func main() {
-	data := ReadFile("test.txt")
+func ReportIsSafe(line []string) bool {
+	var numbers []int
 
-	safeReports := 0
-
-	for _, line := range data {
-		fmt.Println(line)
+	for _, s := range line {
+		num, err := strconv.Atoi(s)
+		if err != nil {
+			panic(fmt.Sprintf("Error converting string %v", num))
+		}
+		numbers = append(numbers, num)
 	}
 
-	fmt.Println("Problem 1: ", safeReports)
+	report := Report(numbers)
+
+	if !report.isOrdered() {
+		return false
+	}
+
+	if !report.HasMaxDifference(3) {
+		return false
+	}
+	return true
+}
+
+func removeAtIndex(levels []string, index int) []string {
+	if index < 0 || index >= len(levels) {
+		return levels
+	}
+	newLevels := make([]string, 0, len(levels)-1)
+	newLevels = append(newLevels, levels[:index]...)
+	return append(newLevels, levels[index+1:]...)
+
+}
+
+func main() {
+	data := ReadFile("input.txt")
+
+	safeReportsProblem1 := 0
+	safeReportsProblem2 := 0
+
+	for _, line := range data {
+		levels := strings.Split(line, " ")
+		if ReportIsSafe(levels) {
+			safeReportsProblem1++
+		}
+
+		for index := range levels {
+			newLevels := removeAtIndex(levels, index)
+			if ReportIsSafe(newLevels) {
+				safeReportsProblem2++
+				break
+			}
+		}
+
+	}
+
+	fmt.Println("Problem 1: ", safeReportsProblem1)
+	fmt.Println("Problem 2: ", safeReportsProblem2)
 }
