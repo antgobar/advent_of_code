613a99f72defd664654cdcf5fbc8981c04cba513
antgobar <antongb09@gmail.com>
2024-12-03 19:59:27 +0000

    tidy

--- 2024/day_3/main.go
+++ 2024/day_3/main.go
@@ -14,12 +14,12 @@ func ReadFile(path string) string {
 	return strings.Replace(data, "\n", "", -1)
 }
 
-func extractMuls(line string) []string {
-	mulMatch := regexp.MustCompile(`mul\((\d+),(\d+)\)`)
-	return mulMatch.FindAllString(line, -1)
+func extractMulMatches(line string) []string {
+	matcher := regexp.MustCompile(`mul\((\d+),(\d+)\)`)
+	return matcher.FindAllString(line, -1)
 }
 
-func MustStr2Int(str string) int {
+func MustConvertStrInt(str string) int {
 	num, err := strconv.Atoi(str)
 	if err != nil {
 		panic(fmt.Sprintf("Error parsing string %v as number", num))
@@ -27,31 +27,32 @@ func MustStr2Int(str string) int {
 	return num
 }
 
-func computeMultSum(muls []string) int {
+func mulCompute(muls []string) int {
 	total := 0
 
 	for _, match := range muls {
-		extractedNums := regexp.MustCompile(`\d+`)
-		nums := extractedNums.FindAllString(match, -1)
+		matcher := regexp.MustCompile(`\d+`)
+		nums := matcher.FindAllString(match, -1)
 
-		total += MustStr2Int(nums[0]) * MustStr2Int(nums[1])
+		total += MustConvertStrInt(nums[0]) * MustConvertStrInt(nums[1])
 	}
 	return total
 }
 
-func removeDontsToDos(data string) string {
+func cleanDonts(data string) string {
 	matcher := regexp.MustCompile(`don't\(([^()]*)\).*?do\(([^()]*)\)`)
 	return matcher.ReplaceAllString(data, "")
 }
 
 func main() {
 	data := ReadFile("input.txt")
-	nums1 := extractMuls(data)
-	totalProblem1 := computeMultSum(nums1)
+
+	nums1 := extractMulMatches(data)
+	totalProblem1 := mulCompute(nums1)
 	fmt.Println("Problem 1: ", totalProblem1)
 
-	cleaned := removeDontsToDos(data)
-	nums2 := extractMuls(cleaned)
-	totalProblem2 := computeMultSum(nums2)
+	cleaned := cleanDonts(data)
+	nums2 := extractMulMatches(cleaned)
+	totalProblem2 := mulCompute(nums2)
 	fmt.Println("Problem 2: ", totalProblem2)
 }
