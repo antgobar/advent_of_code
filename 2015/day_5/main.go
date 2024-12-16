670d570aa7ff1d73a4fd172f713dc03e07ef9446
antgobar <antongb09@gmail.com>
2024-12-16 08:50:35 +0000

    2015 day 5

--- 2015/day_5/main.go
+++ 2015/day_5/main.go
@@ -1,1 +1,40 @@
 package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+func ReadFile(path string) []string {
+	content, err := os.ReadFile(path)
+	if err != nil {
+		panic(fmt.Sprintf("Error reading file: %v", path))
+	}
+	return strings.Split(string(content), "\n")
+}
+
+func atLeastNVowels(str string, n int) bool {
+	count := 0
+	vowels := "aeiou"
+	for _, s := range str {
+		if strings.Contains(vowels, string(s)) {
+			count++
+		}
+	}
+
+	if count >= n {
+		return true
+	}
+	return false
+}
+
+func main() {
+	niceStrings := 0
+	lines := ReadFile("test.txt")
+
+	for _, str := range lines {
+		fmt.Println(str)
+		niceStrings++
+	}
+}
